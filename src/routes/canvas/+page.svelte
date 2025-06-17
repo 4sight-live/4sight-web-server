cd35d9bf8db4dae6ab161d5d36e1c8f30303587f
Taylor King <taylorking206@u.boisestate.edu>
2025-06-17 15:48:13 -0600

    square graphics

---
+++ src/routes/canvas/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { createPage } from '$lib/canvas/front-page';
+    import { Canvas } from 'canvas/canvas';
+	import { Drawable } from 'canvas/drawable';
+	import { onMount } from 'svelte';
+
+    let canvas: HTMLCanvasElement;
+    let c: Canvas | undefined = $state(undefined);
+
+
+
+    onMount(() => {
+        const ctx = canvas.getContext('2d');
+        if (!ctx) throw new Error('Failed to get canvas context');
+
+        c = new Canvas(ctx);
+
+        return createPage(c);
+    });
+</script>
+
+
+<canvas bind:this={canvas} style="width: 100%; aspect-ratio: 2/1;" width=1000 height=500></canvas>
