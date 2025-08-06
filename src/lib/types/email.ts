661b7698db41150aec776f4e0ef73fbd95bbaab7
Taylor King <taylorking206@u.boisestate.edu>
2025-08-06 10:06:15 -0600

    merge main

--- src/lib/types/email.ts
+++ src/lib/types/email.ts
@@ -3,13 +3,21 @@ export type Email = {
 		link: string;
 		supportEmail: string;
 	};
-	'new-user': {
-		username: string;
-		verification: string;
-	};
 	test: {
 		service: string;
 		link: string;
 		linkText: string;
+	};
+	'welcome-mailing-list': {
+		name: string;
+		logo: string;
+		visit: string;
+		unsubscribe: string;
+	};
+	'welcome-onboarding': {
+		logo: string;
+		contributorGuide: string;
+		site: string;
+		unsubscribe: string;
 	};
 };
